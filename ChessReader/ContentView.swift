de8ec41ab5aff9c29655daf814798fa79a6bcc44
David Dahlman <daviddahlman82@gmail.com>
2021-01-25 15:30:06 +0100

    start ExtractedView() for bord

--- ChessReader/ContentView.swift
+++ ChessReader/ContentView.swift
@@ -12,75 +12,12 @@ struct ContentView: View {
     @State var image = "Chess_plt60"
     var body: some View {
         ZStack{
-            
-            VStack(spacing: 0){
-                HStack(spacing: 0){
-                    ZStack{
-                        Color(red: 0.02, green: 0.77, blue: 0.42)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.1, green: 0.1, blue: 0.1)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.02, green: 0.77, blue: 0.42)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.1, green: 0.1, blue: 0.1)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.02, green: 0.77, blue: 0.42)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: .center)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.1, green: 0.1, blue: 0.1)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.02, green: 0.77, blue: 0.42)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                    ZStack{
-                        Color(red: 0.1, green: 0.1, blue: 0.1)
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Image(image)
-                            .resizable()
-                            .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                    }
-                }
+            VStack{
+                // text och annat
+                ExtractedView()
+                // knappar och annat
             }
+            
         }
         
     }
@@ -89,5 +26,80 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    var size: CGFloat = 30
+    @State var image = "Chess_plt60"
+    var body: some View {
+        VStack(spacing: 0){
+            HStack(spacing: 0){
+                ZStack{
+                    Color(red: 0.02, green: 0.77, blue: 0.42)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.1, green: 0.1, blue: 0.1)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.02, green: 0.77, blue: 0.42)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.1, green: 0.1, blue: 0.1)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.02, green: 0.77, blue: 0.42)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: .center)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.1, green: 0.1, blue: 0.1)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.02, green: 0.77, blue: 0.42)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                ZStack{
+                    Color(red: 0.1, green: 0.1, blue: 0.1)
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit).frame(width: size, height: size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+            }
+        }
     }
 }
