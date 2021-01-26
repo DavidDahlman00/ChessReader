494352939a820928b24d298bc8526c5e09572ed2
David Dahlman <daviddahlman82@gmail.com>
2021-01-26 15:41:23 +0100

    fix auto set bord size

--- ChessReader/ContentView.swift
+++ ChessReader/ContentView.swift
@@ -10,25 +10,30 @@ import SwiftUI
 struct ContentView: View {
     @State var bord: [[String]] = [["blackRock", "blackKnight", "blackBishop", "blackKing", "blackQueen", "blackBishop", "blackKnight", "blackRock"], ["blackPawn","blackPawn","blackPawn","blackPawn","blackPawn","blackPawn","blackPawn","blackPawn"], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["whitePawn","whitePawn","whitePawn","whitePawn","whitePawn","whitePawn","whitePawn","whitePawn"], ["whiteRock", "whiteKnight", "whiteBishop", "whiteKing", "whiteQueen", "whiteBishop", "whiteKnight", "whiteRock"]]
     var body: some View {
-        ZStack{
-            VStack{
-                // text och annat
-                BordView(image: bord)
-                Button("Test"){
-                    if bord[3][3] == "" {
-                        bord[3][3] = bord[1][3]
-                        bord[1][3] = ""
-                    }else{
-                        bord[1][3] = bord[3][3]
-                        bord[3][3] = ""
-                     }
+        GeometryReader{geo in
+            ZStack{
+                VStack{
+                    // text och annat
+                    BordView(imageSize: 0.92 * geo.size.width / 8, image: bord)
+                        
+                    Button("Test"){
+                        if bord[3][3] == "" {
+                            bord[3][3] = bord[1][3]
+                            bord[1][3] = ""
+                        }else{
+                            bord[1][3] = bord[3][3]
+                            bord[3][3] = ""
+                         }
+                    }
+                    // knappar och annat
                 }
-                // knappar och annat
+                
             }
             
         }
-        
     }
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -38,23 +43,25 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct BordView: View {
-  
+    let imageSize: CGFloat
     var image: [[String]]
     var body: some View {
         VStack(spacing: 0){
-            RowView(row: 0, image: image[0])
-            RowView(row: 1, image: image[1])
-            RowView(row: 2, image: image[2])
-            RowView(row: 3, image: image[3])
-            RowView(row: 4, image: image[4])
-            RowView(row: 5, image: image[5])
-            RowView(row: 6, image: image[6])
-            RowView(row: 7, image: image[7])
+            RowView(imageSize: imageSize, row: 0, image: image[0])
+            RowView(imageSize: imageSize, row: 1, image: image[1])
+            RowView(imageSize: imageSize, row: 2, image: image[2])
+            RowView(imageSize: imageSize, row: 3, image: image[3])
+            RowView(imageSize: imageSize, row: 4, image: image[4])
+            RowView(imageSize: imageSize, row: 5, image: image[5])
+            RowView(imageSize: imageSize, row: 6, image: image[6])
+            RowView(imageSize: imageSize, row: 7, image: image[7])
         }
+        .padding()
     }
 }
 
 struct RowView: View {
+    let imageSize: CGFloat
     let row: Int
     let image: [String]
     var color1: Color{
@@ -75,21 +82,21 @@ struct RowView: View {
     
     var body: some View {
         HStack(spacing: 0){
-            SquareView(color: color1, pice: image[0])
-            SquareView(color: color2, pice: image[1])
-            SquareView(color: color1, pice: image[2])
-            SquareView(color: color2, pice: image[3])
-            SquareView(color: color1, pice: image[4])
-            SquareView(color: color2, pice: image[5])
-            SquareView(color: color1, pice: image[6])
-            SquareView(color: color2, pice: image[7])
+            SquareView(size: imageSize, color: color1, pice: image[0])
+            SquareView(size: imageSize, color: color2, pice: image[1])
+            SquareView(size: imageSize, color: color1, pice: image[2])
+            SquareView(size: imageSize, color: color2, pice: image[3])
+            SquareView(size: imageSize, color: color1, pice: image[4])
+            SquareView(size: imageSize, color: color2, pice: image[5])
+            SquareView(size: imageSize, color: color1, pice: image[6])
+            SquareView(size: imageSize, color: color2, pice: image[7])
 
         }
     }
 }
 
 struct SquareView: View {
-    var size: CGFloat = 44
+    var size: CGFloat
     let color: Color
     var pice: String
     var body: some View {
