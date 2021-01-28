678b41a64a1bc95af51b81024bc6662a641508d1
axelsode <axel.soderber1996@gmai.com>
2021-01-28 12:29:15 +0100

    gh

--- ChessReader/ContentView.swift
+++ ChessReader/ContentView.swift
@@ -9,30 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var gameList = GameList()
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+    }
+    
      var body: some View {
         
            
          NavigationView{
             ZStack{
+                Color(red: 14.0/255.0, green: 14.0/255.0, blue: 31.0/255.0).edgesIgnoringSafeArea(.all)
              List() {
                  ForEach(gameList.entries){ entry in
                     NavigationLink(destination: ChessBordView()){
                          ListRowView(entry: entry)
+                            
                      }
+                    .listRowBackground(Color(red: 14.0/255.0, green: 14.0/255.0, blue: 31.0/255.0))
+                    
                  }.onDelete(perform: { indexSet in
                      gameList.entries.remove(atOffsets: indexSet)
                  })
-             }.listRowBackground(Color.red)
+             }
              
-             
+             .foregroundColor(.gray)
              .navigationBarTitle("Chess Reader")
              .navigationBarItems(trailing: NavigationLink(destination: ChessBordView(), label: {Image(systemName: "plus.circle")}))
              
+            }.foregroundColor(.red)
          }
-            
-                .edgesIgnoringSafeArea(.all)
-        }
-
+         
      }
 }
 
