edfd681fec6ed511d7a3936d0bc4688bd9bfea06
axelsode <axel.soderber1996@gmai.com>
2021-02-02 15:33:49 +0100

    added firestore to app

---
+++ Pods/Target Support Files/Pods-ChessReader/Pods-ChessReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChessReader : NSObject
+@end
+@implementation PodsDummy_Pods_ChessReader
+@end
---
+++ Pods/Target Support Files/Pods-ChessReaderTests/Pods-ChessReaderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChessReaderTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChessReaderTests
+@end
