915a8b355ea401f6d3ce5e4d9d2352ce018f527f
David Dahlman <daviddahlman82@gmail.com>
2021-02-10 11:22:11 +0100

    init Auth

--- ChessReader/ContentView.swift
+++ ChessReader/ContentView.swift
@@ -6,24 +6,24 @@
 //
 
 import SwiftUI
-//import FirebaseAuth
+import FirebaseAuth
 
 struct ContentView: View {
-//    let auth = Auth.auth()
-//
-//    init() {
-//        print("###########")
-//        auth.signInAnonymously{ (result, err) in
-//            print("!!!!!!!!")
-//            if let err = err {
-//
-//                print(err.localizedDescription)
-//                print("!!!!!!!!!!")
-//                return
-//            }
-//            print("Success Auth")
-//        }
-//    }
+    let auth = Auth.auth()
+
+    init() {
+        print("###########")
+        auth.signInAnonymously{ (result, err) in
+            print("!!!!!!!!")
+            if let err = err {
+
+                print(err.localizedDescription)
+                print("!!!!!!!!!!")
+                return
+            }
+            print("Success Auth")
+        }
+    }
     var body: some View {
         //Image("chessImage")
         TabView{
