3d1d3654bfc5472c7aa3e083a85620b6b3e841f5
David Dahlman <daviddahlman82@gmail.com>
2021-02-16 12:59:43 +0100

    first test read PGN

--- ChessReader/ChessBordView.swift
+++ ChessReader/ChessBordView.swift
@@ -26,25 +26,21 @@ struct ChessBordView : View {
                     BordView(bord: bord, imageSize: 0.92 * geo.size.width / 8, image: bord.bord, action: "ChessBordView")
                     HStack{
                         Button(action: {
-                            if bord.bord[3][3] == "" {
-                                bord.bord[3][3] = bord.bord[1][3]
-                                bord.bord[1][3] = ""
-                            }else{
-                                bord.bord[1][3] = bord.bord[3][3]
-                                bord.bord[3][3] = ""
-                             }
+                            if bord.testPGNInt > 0 {
+                                bord.testPGNInt = bord.testPGNInt - 1
+                                print("move back")
+                                print(bord.testPGN[bord.testPGNInt])
+                            }
                         }) {
                             Image(systemName: "backward.fill")
                         }
                         .foregroundColor(.gray)
                         
                         Button(action: {
-                            if bord.bord[4][3] == "" {
-                                bord.bord[4][3] = bord.bord[6][3]
-                                bord.bord[6][3] = ""
-                            }else{
-                                bord.bord[6][3] = bord.bord[4][3]
-                                bord.bord[4][3] = ""
+                            if bord.testPGNInt < bord.testPGN.count - 1{
+                                bord.testPGNInt = bord.testPGNInt + 1
+                                print("move forward")
+                                print(bord.testPGN[bord.testPGNInt])
                              }
                         }) {
                             Image(systemName: "forward.fill")
