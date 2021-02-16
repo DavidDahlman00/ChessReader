47767b08368398265f182063f2e65ef647e79896
David Dahlman <daviddahlman82@gmail.com>
2021-02-16 17:29:01 +0100

    not working read fun in readPGN

--- ChessReader/ChessBordView.swift
+++ ChessReader/ChessBordView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ChessBordView : View {
     var playedGame : GameListEntry? = nil
     var testText: String = "Test"
-
+    var game = ReadPGN()
     
    @ObservedObject var bord = Bord()
     var body: some View {
@@ -26,22 +26,29 @@ struct ChessBordView : View {
                     BordView(bord: bord, imageSize: 0.92 * geo.size.width / 8, image: bord.bord, action: "ChessBordView")
                     HStack{
                         Button(action: {
-                            if bord.testPGNInt > 0 {
-                                bord.testPGNInt = bord.testPGNInt - 1
-                                print("move back")
-                                print(bord.testPGN[bord.testPGNInt])
-                            }
+                            game.moveBackward()
+                            print(game.testPGN[game.testPGNInt])
+                            
                         }) {
                             Image(systemName: "backward.fill")
                         }
                         .foregroundColor(.gray)
                         
                         Button(action: {
-                            if bord.testPGNInt < bord.testPGN.count - 1{
-                                bord.testPGNInt = bord.testPGNInt + 1
-                                print("move forward")
-                                print(bord.testPGN[bord.testPGNInt])
-                             }
+                            print(game.testPGNInt)
+                            print(game.testPGN[game.testPGNInt])
+                            game.readGame()
+                            print(game.information)
+                            print(game.lightMoveList.count)
+                            print(game.darkMoveList.count)
+                            for move in game.lightMoveList{
+                                print(move)
+                            }
+                            print("====================")
+                            for move in game.darkMoveList{
+                                print(move)
+                            }
+                            game.moveForward()
                         }) {
                             Image(systemName: "forward.fill")
                         }
